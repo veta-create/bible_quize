304dd6f04630f874b324d6662387465c8997880b
@veta-create <vita.eksuzyan@bk.ru>
2022-09-04 10:33:22 +0300

    redesign back button

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const facts = document.querySelector(".facts");
-  const story = document.querySelector(".story");
-  const blockFacts = document.querySelector(".block-facts");
-  const blockStory = document.querySelectorAll(".block-story");
+  const quize = document.querySelector('.quize')
+  const greeting = document.querySelector('.greeting')
+  const form = document.querySelector('form')
 
-  facts.addEventListener("click", function() {
-    facts.style.display = "block";
-    blockFacts.style.display = "block";
-  })
+  form.style.display = 'none'
+  greeting.style.display = 'block'
 
-  story.addEventListener("click", function() {
-    facts.style.display = "block";
-    story.style.display = "block";
-    blockFacts.style.display = "block";
-    for(let story of blockStory) {
-      story.style.display = "block";
-    }
-  })
 })
